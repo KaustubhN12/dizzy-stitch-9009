30c02360d065e0503bbdbce96acd40257a5713bd
Kaustubh Nawade <112754493+KaustubhN12@users.noreply.github.com>
2023-04-02 14:36:00 +0530

    almost-done

---
+++ frontend/src/Pages/UserAccount/Promotion.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Text } from '@chakra-ui/react'
+
+const Promotion = () => {
+  return (
+    <div style={{ backgroundColor: "#e1e9ec", height: "auto" , paddingBottom:"30px" }}>
+      
+      <Box className="account-Nav" marginBottom="25px">
+           <Box width="83%" margin={"auto"}>
+             <Box width="60%" display="flex" justifyContent="space-evenly" >
+             <Link to="/orders"><Box padding="15px" ><Text >My orders</Text></Box></Link>
+                <Link to="/credits"><Box padding="15px"><Text fontSize="sm">Credits</Text></Box></Link>
+                <Link to="/profile"><Box padding="15px"><Text fontSize="sm">Profile</Text></Box></Link>
+                <Link to="/newsletter"><Box padding="15px"><Text fontSize="sm">Newsletters</Text></Box></Link>
+                <Link to="/refer"><Text><Box padding="15px"><Text fontSize="sm">Refer a friend</Text></Box></Text></Link>
+                <Link to="/promotion"><Box padding="15px"borderBottom="2px solid #ef534e"><Text fontSize="sm" color="#ef534e">Promotions</Text></Box></Link>
+             </Box>
+           </Box>
+      </Box>
+
+      <Box width="83%" margin={"auto"}>
+        <Text marginLeft="30px" fontSize="23px" fontWeight="thin">My Promos</Text>
+      </Box>
+
+    </div>
+  )
+}
+
+export default Promotion
--- frontend/src/Routes/AllRoutes.jsx
+++ frontend/src/Routes/AllRoutes.jsx
@@ -1,9 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-
 import Admin from "../Pages/Admin";
-
-
 import Login from "../Pages/Login";
 import Home from "../Pages/Homepage/Home";
 import GiftCards from "../Pages/GiftCards";
@@ -11,16 +8,20 @@ import Payment from "../Pages/Payment/Payment";
 import PremiumRestaurant from "../Pages/Restaurant/PremiumRestaurant";
 import RestaurantHome from "../Pages/Restaurant/RestaurantHome";
 import Signup from "../Pages/Signup";
-
 import SingleCompany from "../Pages/SingleCompany";
 import SingleHealthAdmin from "../Pages/SingleHealthAdmin";
 import SingleSalonAdmin from "../Pages/SingleSalonAdmin";
 import SingleGiftAdmin from "../Pages/SingleGiftAdmin";
-
-
 import AdminLogin from "../Pages/AdminLogin";
 import Health from "../Pages/Health";
 import NightLife from "../Pages/NightLife";
+import Orders from "../Pages/UserAccount/Orders";
+import Credits from "../Pages/UserAccount/Credits";
+import Profile from "../Pages/UserAccount/Profile";
+import NewsLetter from "../Pages/UserAccount/NewsLetter";
+import Refer from "../Pages/UserAccount/Refer";
+import Promotion from "../Pages/UserAccount/Promotion";
+import Salon from "../Pages/salon";
 
 
 const AllRoutes = () => {
@@ -37,16 +38,20 @@ const AllRoutes = () => {
       <Route path="/updatehealth/:id" element={<SingleHealthAdmin />} />
       <Route path="/updatesalon/:id" element={<SingleSalonAdmin />} />
       <Route path="/updategift/:id" element={<SingleGiftAdmin />} />
-      <Route
-        path="/restaurant/premium-restaurant"
-        element={<PremiumRestaurant />}
-      />
-      <Route path="/singledetails" element={<Singledetails />}></Route>
-      <Route path="/singledetails/:userID" element={<Singledetails />}></Route>
+      <Route path="/restaurant/premium-restaurant" element={<PremiumRestaurant />}/>
+      {/* <Route path="/singledetails" element={<Singledetails />}></Route> */}
+      {/* <Route path="/singledetails/:userID" element={<Singledetails />}></Route> */}
       <Route path="/giftcards" element={<GiftCards />} />
       <Route path="/health" element={<Health />} />
       <Route path="/nightlife" element={<NightLife />} />
       <Route path="/restaurant" element={<RestaurantHome />} />
+      <Route path="/orders" element={<Orders />} />
+      <Route path="/credits" element={<Credits />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/newsletter" element={<NewsLetter />} />
+      <Route path="/refer" element={<Refer />} />
+      <Route path="/promotion" element={<Promotion />} />
+      <Route path="/salon" element={<Salon />} />
     </Routes>
   );
 };
